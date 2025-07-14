2bfb4ce8de761e1ab23dc4dd2fb869d4b5cbd77e
satv2025 <eljuanchodelavida@gmail.com>
2025-07-14 17:24:42 -0300

    pohl

--- src/embed.js
+++ src/embed.js
@@ -5,7 +5,7 @@ import SATVPlayer from "./SATVPlayer.jsx";
 window.SATVPlayerEmbed = function ({ elementId, videoUrl }) {
   const el = document.getElementById(elementId);
   if (el) {
-    ReactDOM.render(<SATVPlayer src={videoUrl} />, el);
+    ReactDOM.render(<SATVPlayer videoUrl={videoUrl} />, el);
   } else {
     console.error("Elemento no encontrado:", elementId);
   }
